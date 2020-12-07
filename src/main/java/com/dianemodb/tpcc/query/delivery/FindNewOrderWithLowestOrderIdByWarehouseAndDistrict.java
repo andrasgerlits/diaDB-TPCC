824980044f6fc54790b2431c12ef87667418ebe8
U-Andras-Levono\Andras <andras.gerlits@gmail.com>
2020-12-07 13:23:17 +0100

    Fix for aggregate queries

--- src/main/java/com/dianemodb/tpcc/query/delivery/FindNewOrderWithLowestOrderIdByWarehouseAndDistrict.java
+++ src/main/java/com/dianemodb/tpcc/query/delivery/FindNewOrderWithLowestOrderIdByWarehouseAndDistrict.java
@@ -1,11 +1,6 @@
 package com.dianemodb.tpcc.query.delivery;
 
-import java.util.List;
-
-import com.dianemodb.RecordWithVersion;
 import com.dianemodb.h2impl.SingleIndexSingleParameterSetQueryDistributionPlan;
-import com.dianemodb.metaschema.QueryStep;
-import com.dianemodb.metaschema.RecordColumn;
 import com.dianemodb.metaschema.distributed.AggregateFunction;
 import com.dianemodb.metaschema.distributed.AggregateType;
 import com.dianemodb.tpcc.entity.NewOrders;
@@ -14,26 +9,14 @@ import com.dianemodb.tpcc.schema.NewOrdersTable;
 public class FindNewOrderWithLowestOrderIdByWarehouseAndDistrict extends SingleIndexSingleParameterSetQueryDistributionPlan<NewOrders>{
 
 	public static final String ID = "findNewOrdersByDistrictAndWarehouse";
-	private static final RecordColumn<NewOrders, Integer> AGGREGATE_COLUMN = NewOrdersTable.ORDER_ID_COLUMN;
 	
 	public FindNewOrderWithLowestOrderIdByWarehouseAndDistrict(NewOrdersTable table) {
 		super(
 			ID, 
 			table, 
 			table.getCompositeIndex(),
-			new AggregateFunction(AGGREGATE_COLUMN, AggregateType.MIN) 
+			new AggregateFunction<>(NewOrdersTable.ORDER_ID_COLUMN, AggregateType.MIN) 
 		);
 	}
-	
-	@Override
-	public List<RecordWithVersion<NewOrders>> aggregateResults(List<RecordWithVersion<NewOrders>> results) {
-		// find highest of the reverse comparator (so lowest)
-		return QueryStep.findHighest(
-				results, 
-				(one, other) -> Integer.compare(
-									AGGREGATE_COLUMN.get(other.getRecord()), 
-									AGGREGATE_COLUMN.get(one.getRecord())
-								)
-		);
-	}
+
 }
