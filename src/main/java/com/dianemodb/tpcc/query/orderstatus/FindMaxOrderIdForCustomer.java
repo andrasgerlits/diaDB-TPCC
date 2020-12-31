c95675f07d491550c0a09a52341a33c5e400ba96
U-Andras-Levono\Andras <andras.gerlits@gmail.com>
2020-12-31 12:34:08 +0100

    minor fix around queries

--- src/main/java/com/dianemodb/tpcc/query/orderstatus/FindMaxOrderIdForCustomer.java
+++ src/main/java/com/dianemodb/tpcc/query/orderstatus/FindMaxOrderIdForCustomer.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.commons.lang3.tuple.Pair;
 
 import com.dianemodb.h2impl.SingleParameterSetQueryDistributionPlan;
+import com.dianemodb.metaschema.distributed.Condition;
 import com.dianemodb.metaschema.distributed.LimitClause;
 import com.dianemodb.metaschema.distributed.OrderByClause;
 import com.dianemodb.metaschema.distributed.OrderByClause.OrderType;
@@ -24,14 +25,27 @@ import com.dianemodb.tpcc.schema.OrdersTable;
 public class FindMaxOrderIdForCustomer extends SingleParameterSetQueryDistributionPlan<Orders> {
 
 	private static final LimitClause LIMIT_CLAUSE = new LimitClause(1);
-	private static final OrderByClause<Orders> ORDER_BY_CLAUSE = new OrderByClause<>(List.of(Pair.of(OrdersTable.ORDER_ID_COLUMN, OrderType.DESC)));
+	
+	private static final OrderByClause<Orders> ORDER_BY_CLAUSE = 
+			new OrderByClause<>(List.of(Pair.of(OrdersTable.ORDER_ID_COLUMN, OrderType.DESC)));
+	
 	public static final String ID = "findMaxOrderIdForCustomer";
+
+	private static final Condition<Orders> CONDITION = 			
+			Condition.andEqualsEach(
+				List.of(
+					OrdersTable.WAREHOUSE_ID_COLUMN,
+					OrdersTable.DISTRICT_ID_COLUMN,
+					OrdersTable.CUSTOMER_ID_COLUMN
+				)
+			);
 
 	public FindMaxOrderIdForCustomer(OrdersTable table) {
 		super(
 			ID,  
 			table, 
 			table.getCompositeCustomerIndex(),
+			CONDITION,
 			ORDER_BY_CLAUSE,
 			LIMIT_CLAUSE
 		);
